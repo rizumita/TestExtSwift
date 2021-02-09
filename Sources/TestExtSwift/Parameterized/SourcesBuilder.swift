4588c739c2a6b176459cc573710b878f6af59537
Ryoichi Izumita <r.izumita@caph.jp>
2021-02-09 21:45:18 +0900

    Add multiple values support to SourcesBuilder

--- Sources/TestExtSwift/Parameterized/SourcesBuilder.swift
+++ Sources/TestExtSwift/Parameterized/SourcesBuilder.swift
@@ -33,6 +33,14 @@ public struct SourcesBuilder {
         array
     }
 
+    public static func buildBlock<A, B>(_ rows: Row<(A, B)>...) -> [Row<(A, B)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B>(_ rowArray: [Row<(A, B)>]...) -> [Row<(A, B)>] {
+        rowArray.flatMap { $0 }
+    }
+
     public static func buildBlock<A, B, C>(_ array: [(A, B, C)]...) -> [(A, B, C)] {
         array.flatMap { $0 }
     }
@@ -41,11 +49,75 @@ public struct SourcesBuilder {
         array
     }
 
+    public static func buildBlock<A, B, C>(_ rows: Row<(A, B, C)>...) -> [Row<(A, B, C)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B, C>(_ rowArray: [Row<(A, B, C)>]...) -> [Row<(A, B, C)>] {
+        rowArray.flatMap { $0 }
+    }
+
     public static func buildBlock<A, B, C, D>(_ array: [(A, B, C, D)]...) -> [(A, B, C, D)] {
         array.flatMap { $0 }
     }
 
     public static func buildBlock<A, B, C, D>(_ array: (A, B, C, D)...) -> [(A, B, C, D)] {
         array
+    }
+
+    public static func buildBlock<A, B, C, D>(_ rows: Row<(A, B, C, D)>...) -> [Row<(A, B, C, D)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B, C, D>(_ rowArray: [Row<(A, B, C, D)>]...) -> [Row<(A, B, C, D)>] {
+        rowArray.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E>(_ array: [(A, B, C, D, E)]...) -> [(A, B, C, D, E)] {
+        array.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E>(_ array: (A, B, C, D, E)...) -> [(A, B, C, D, E)] {
+        array
+    }
+
+    public static func buildBlock<A, B, C, D, E>(_ rows: Row<(A, B, C, D, E)>...) -> [Row<(A, B, C, D, E)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B, C, D, E>(_ rowArray: [Row<(A, B, C, D, E)>]...) -> [Row<(A, B, C, D, E)>] {
+        rowArray.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E, F>(_ array: [(A, B, C, D, E, F)]...) -> [(A, B, C, D, E, F)] {
+        array.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E, F>(_ array: (A, B, C, D, E, F)...) -> [(A, B, C, D, E, F)] {
+        array
+    }
+
+    public static func buildBlock<A, B, C, D, E, F>(_ rows: Row<(A, B, C, D, E, F)>...) -> [Row<(A, B, C, D, E, F)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B, C, D, E, F>(_ rowArray: [Row<(A, B, C, D, E, F)>]...) -> [Row<(A, B, C, D, E, F)>] {
+        rowArray.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E, F, G>(_ array: [(A, B, C, D, E, F, G)]...) -> [(A, B, C, D, E, F, G)] {
+        array.flatMap { $0 }
+    }
+
+    public static func buildBlock<A, B, C, D, E, F, G>(_ array: (A, B, C, D, E, F, G)...) -> [(A, B, C, D, E, F, G)] {
+        array
+    }
+
+    public static func buildBlock<A, B, C, D, E, F, G>(_ rows: Row<(A, B, C, D, E, F, G)>...) -> [Row<(A, B, C, D, E, F, G)>] {
+        rows
+    }
+
+    public static func buildBlock<A, B, C, D, E, F, G>(_ rowArray: [Row<(A, B, C, D, E, F, G)>]...) -> [Row<(A, B, C, D, E, F, G)>] {
+        rowArray.flatMap { $0 }
     }
 }
