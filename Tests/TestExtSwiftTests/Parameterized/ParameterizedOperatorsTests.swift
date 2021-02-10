f33fba9dc6db54d02256a6cd5bc576fe2464ebfa
Ryoichi Izumita <r.izumita@caph.jp>
2021-02-10 10:59:37 +0900

    Update testing

--- Tests/TestExtSwiftTests/Parameterized/ParameterizedOperatorsTests.swift
+++ Tests/TestExtSwiftTests/Parameterized/ParameterizedOperatorsTests.swift
@@ -12,33 +12,33 @@ import XCTest
 class ParameterizedOperatorsTests: XCTestCase {
 
     func testParam() throws {
-        try {
-                ^{ 1; 3; false }
-                ^{ 2; 3; true }
-                ^{ 3; 3; false }
-            } =>
-            Parameterized("Parameterized Testing")
-                .before { p1, p2, p3 in print("before") }
-                .after { p1, p2, p3 in print("after") }
-                .test { _, p1, p2, p3, line in
-                    XCTAssertEqual(p1 + 1 == p2, p3, line: line)
-                }
+        try
+        {
+            r { 1; 3; false }
+            r { 2; 3; true }
+            r { 3; 3; false }
+        } => Parameterized("Parameterized Testing")
+            .before { p1, p2, p3 in print("before") }
+            .after { p1, p2, p3 in print("after") }
+            .test { _, p1, p2, p3, line in
+                XCTAssertEqual(p1 + 1 == p2, p3, line: line)
+            }
     }
 
     func testParam_Rows() throws {
-        try [^{ 1; 3; false },
-             ^{ 2; 3; true },
-             ^{ 3; 3; false }] =>
-            Parameterized("Parameterized Testing")
-                .test { _, p1, p2, p3, line in
-                    XCTAssertEqual(p1 + 1 == p2, p3, line: line)
-                }
+        try
+        [r { 1; 3; false },
+         r { 2; 3; true },
+         r { 3; 3; false }] => Parameterized("Parameterized Testing")
+            .test { _, p1, p2, p3, line in
+                XCTAssertEqual(p1 + 1 == p2, p3, line: line)
+            }
     }
 
     func testParam_Seq() throws {
-        try [1, 2, 3] =>
-            Parameterized("Parameterized Testing")
-                .test { _, p in XCTAssertEqual(p, p) }
+        try
+        [1, 2, 3] => Parameterized("Parameterized Testing")
+            .test { _, p in XCTAssertEqual(p, p) }
     }
 
     func testParam_Product() throws {
